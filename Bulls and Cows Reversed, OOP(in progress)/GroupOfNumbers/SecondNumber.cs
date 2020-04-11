803321c46ef24d32c90c102465a2acfce18e7353
Stefan Koev <stefan.m.koev@gmail.com>
2020-04-11 10:36:17 +0300

    Add files via upload

---
+++ Bulls and Cows Reversed, OOP(in progress)/GroupOfNumbers/SecondNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First_atempt
+{
+    public class SecondNumber
+    {
+        public SecondNumber(List<int> digits, int bullsCount, int cowsCount, int total)
+        {
+            this.Digits = digits;
+            this.BullsCount = bullsCount;
+            this.CowsCount = cowsCount;
+            this.Total = total;
+        }
+
+        public List<int> Digits { get; set; }
+        public int BullsCount { get; set; }
+        public int CowsCount { get; set; }
+        public int Total { get; set; }
+    }
+}
---
+++ Bulls and Cows Reversed, OOP(in progress)/Program.cs
@@ -0,0 +1,37 @@
+﻿using First_atempt.NumberGenerators;
+using System;
+using System.Collections.Generic;
+
+namespace First_atempt
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var firstNum = FirstNumberGenerator.Generate();
+            var secondNum = SecondNumberGenerator.Generate(firstNum);
+            var missingNum = SingleNumberGenerator.Generate(firstNum, secondNum);
+
+            Console.WriteLine(string.Join("", firstNum));
+            int firstNumBulls = int.Parse(Console.ReadLine());
+            int firstNumCows = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join("", secondNum));
+            int secondNumBulls = int.Parse(Console.ReadLine());
+            int secondNumCows = int.Parse(Console.ReadLine());
+
+            var firstNumToWork = new FirstNumber(firstNum, firstNumBulls, firstNumCows, firstNumBulls + firstNumCows);
+            var secondNumToWork = new SecondNumber(secondNum, secondNumBulls, secondNumCows, secondNumBulls + secondNumCows);
+
+            
+
+        }
+        public void FindNumberWithLessBullsAndCows(int firstNumber, int secondNumber)
+        {
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine(string.Join("", firstNumber));
+            }
+        }
+    }
+}
